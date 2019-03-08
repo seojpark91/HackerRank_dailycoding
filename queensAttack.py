de59a40bec9c966dad615cb5dfa29dff9e01bebd
Seo Jung Park <seojpark91@gmail.com>
2019-03-08 09:22:31 +0900

    added test code and debugging when obstacle is none

--- queensAttack.py
+++ queensAttack.py
@@ -1,4 +1,4 @@
-def queensAttack(n, k, r_q, c_q, obstacles):
+def queensAttack(n, k, r_q, c_q, obstacles=None):
     
     # first calculate distance between queen and the edges of the board in all directions
     u = n - r_q
@@ -10,28 +10,31 @@ def queensAttack(n, k, r_q, c_q, obstacles):
     lu = min(l,u)
     ld = min(l,d)
     
+    if obstacles:
     # check if obstacle is in-line with the queen's position in any direction
-    for o in obstacles:
-        if o[1] == c_q: #if they are in-line, calculate distance between them
-            if o[0] < r_q: # if distance is less than the current position of the queen, set the current distance to new minimum
-                d = min(d, r_q-1-o[0])
-            else:
-                u = min(u, o[0]-r_q-1)
-        elif o[0] == r_q:
-            if o[1] < c_q: 
-                l = min(l, c_q-1-o[1])
-            else: 
-                r = min(r, o[1]-c_q-1)
-        elif abs(o[0]-r_q) == abs(o[1]-c_q):
-            if o[1]>c_q:
-                if o[0]>r_q: 
-                    ru = min(ru, o[1]-c_q-1)
+        for o in obstacles:
+            if o[1] == c_q: #if they are in-line, calculate distance between them
+                if o[0] < r_q: # if distance is less than the current position of the queen, set the current distance to new minimum
+                    d = min(d, r_q-1-o[0])
+                else:
+                    u = min(u, o[0]-r_q-1)
+            elif o[0] == r_q:
+                if o[1] < c_q: 
+                    l = min(l, c_q-1-o[1])
                 else: 
-                    rd = min(rd, o[1]-c_q-1)
-            else:
-                if o[0]>r_q: 
-                    lu = min(lu, c_q-1-o[1])
-                else: 
-                    ld = min(ld, c_q-1-o[1])
+                    r = min(r, o[1]-c_q-1)
+            elif abs(o[0]-r_q) == abs(o[1]-c_q):
+                if o[1]>c_q:
+                    if o[0]>r_q: 
+                        ru = min(ru, o[1]-c_q-1)
+                    else: 
+                        rd = min(rd, o[1]-c_q-1)
+                else:
+                    if o[0]>r_q: 
+                        lu = min(lu, c_q-1-o[1])
+                    else: 
+                        ld = min(ld, c_q-1-o[1])
                 
     return u + d + r + l + ru + rd + lu + ld # sum up all the distances for all directions
+
+queensAttack(5, 3, 4, 3, [[5,5],[4,2],[2,3]]), queensAttack(1, 0, 1, 1), queensAttack(4, 0, 4, 4)
